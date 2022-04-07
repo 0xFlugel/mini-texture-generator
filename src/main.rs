b90cdb3002e7cdf3f03d9503bf30d7b157ab83fc
0xFlugel <0xflugel@mailbox.org>
2022-04-07 11:32:31 +0200

    Revert "Merge branch 'layout-system' into redo"
    This reverts commit 02153143323360c899ba0c50b682e8bcd50d34de, reversing
    changes made to 3713de4fb1552f7350e5d77d64a7a30486c392be.

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,8 @@
 
 mod connection_management;
 mod interaction;
-mod layout;
 
 use crate::interaction::InteractionPlugin;
-use crate::layout::{LayoutChildren, LayoutContainerBundle, LayoutPlugin, UiSize};
 use bevy::prelude::*;
 use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
 use bevy::sprite::Mesh2dHandle;
@@ -63,7 +61,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(InteractionPlugin)
-        .add_plugin(LayoutPlugin)
         .add_startup_system(setup)
         .add_system(create_element)
         .add_system(connection_management::start_connecting)
@@ -255,7 +252,6 @@ fn setup(
 
     cmds.spawn_bundle(OrthographicCameraBundle::new_2d())
         .insert(RayCastSource::<MyRaycastSet>::new());
-    //TODO remove?
     cmds.insert_resource(DefaultPluginState::<MyRaycastSet>::default().with_debug_cursor());
 
     // Create sidebar
@@ -291,16 +287,15 @@ fn setup(
         Color::YELLOW_GREEN,
     ];
 
-    let mut children = vec![];
     for (i, effect) in EffectType::all().iter().enumerate() {
         let num = (3 * n + 1) as f32;
         let offset = (3 * i + 1) as f32;
         let transform = transform_from_rect(
             Rect {
-                top: (1.0 - 2.0 * (offset / num)) * y_scale,
-                bottom: (1.0 - 2.0 * ((offset + 2.0) / num)) * y_scale,
-                left: -0.8 * x_scale,
-                right: 0.8 * x_scale,
+                top: 1.0 - 2.0 * (offset / num),
+                bottom: 1.0 - 2.0 * ((offset + 2.0) / num),
+                left: -0.8,
+                right: 0.8,
             },
             1,
         );
@@ -316,12 +311,7 @@ fn setup(
         );
         cmds.entity(child).insert(SidebarElement(*effect));
         cmds.entity(sidebar).add_child(child);
-        children.push(child);
     }
-    cmds.entity(sidebar).insert_bundle(LayoutContainerBundle {
-        children: LayoutChildren(children),
-        ..Default::default()
-    });
 }
 
 /// Create new new entity that is a pipeline element.
@@ -381,7 +371,6 @@ fn create_pipeline_element(
         })
         .insert(MyInteraction::default())
         .insert(RayCastMesh::<MyRaycastSet>::default())
-        .insert(UiSize(Size::new(200.0, 100.0)))
         .id();
     let text_color = {
         let background = materials
@@ -394,7 +383,8 @@ fn create_pipeline_element(
     };
 
     // Add label.
-    let label = create_text(cmds, label, text_color, &font);
+    let label = create_text(cmds, label, text_color, transform, &font);
+    cmds.entity(element).add_child(label);
 
     // Add inputs and outputs.
     let mut inputs = vec![];
@@ -428,10 +418,6 @@ fn create_pipeline_element(
         }
     }
     cmds.entity(element)
-        .insert_bundle(LayoutContainerBundle {
-            children: LayoutChildren(vec![label]),
-            ..Default::default()
-        })
         .push_children(&inputs)
         .push_children(&outputs)
         .insert(InputConnectors(inputs))
@@ -444,6 +430,7 @@ fn create_text(
     cmds: &mut Commands,
     label: &str,
     color: Color,
+    transform: Transform,
     font: &Handle<Font>,
 ) -> Entity {
     let sections = label
@@ -458,7 +445,8 @@ fn create_text(
         })
         .collect();
     cmds.spawn_bundle(Text2dBundle {
-        transform: Transform::from_scale(Vec2::from(TEXT_SCALING).extend(1.0))
+        transform: transform
+            .with_scale(Vec2::from(TEXT_SCALING).extend(1.0))
             // Move inner text clearly in front. 0.5 layers to not collide with a full layer in
             // front (if something is placed on that layer) while being bit-exact.
             .with_translation(Vec3::new(0.0, 0.0, 0.5)),
@@ -471,8 +459,6 @@ fn create_text(
         },
         ..Default::default()
     })
-    // Will be set by the layout system.
-    .insert(UiSize(Size::default()))
     .id()
 }
 
