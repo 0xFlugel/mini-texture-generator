632180d7b1906b29615a2366a046ada1ef4896bc
0xFlugel <0xflugel@mailbox.org>
2022-04-10 13:24:52 +0200

    Add fix for missing update to GPU.

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,12 @@ mod text_entry;
 use crate::connection_management::Connection;
 use crate::interaction::InteractionPlugin;
 use crate::text_entry::{TextEntryPlugin, TextValue, ValueBinding};
+use bevy::ecs::event::{Events, ManualEventReader};
 use bevy::math::XY;
 use bevy::prelude::*;
 use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
 use bevy::sprite::Mesh2dHandle;
+use bevy::utils::HashSet;
 use bevy_mod_raycast::{DefaultPluginState, RayCastMesh, RayCastSource};
 use connection_management::{InputConnector, InputConnectors, OutputConnector, OutputConnectors};
 use interaction::{Draggable, Dragging, MousePosition, MyInteraction, MyRaycastSet};
@@ -73,6 +75,7 @@ fn main() {
         .add_system(connection_management::highlight_connection_acceptor)
         .add_system(connection_management::finish_connection)
         .add_system(update_texture)
+        .add_system(image_modified_detection)
         .run();
 }
 
@@ -193,6 +196,77 @@ fn update_texture(
                     };
                     write_pixel(image, XY { x, y }, color);
                 }
+            }
+        }
+    }
+}
+
+/// Update all [ColorMaterial]s when their [Image] was modified.
+///
+/// This is currently not done automatically by Bevy
+/// ([which is a bug](https://github.com/bevyengine/bevy/pull/3737)). Code copied from the link.
+///
+/// Whenever an [Image] is modified, if it is referenced by a [ColorMaterial]
+/// we need to trigger an [AssetEvent::Modified] for the [ColorMaterial]
+/// so that extract_render_assets detects it and calls for a new extraction
+fn image_modified_detection(
+    mut image_to_material: Local<HashMap<Handle<Image>, HashSet<Handle<ColorMaterial>>>>,
+    mut image_events: EventReader<AssetEvent<Image>>,
+    materials: Res<Assets<ColorMaterial>>,
+    mut material_events_reader: Local<ManualEventReader<AssetEvent<ColorMaterial>>>,
+    mut material_events: ResMut<Events<AssetEvent<ColorMaterial>>>,
+) {
+    // Keep the image_to_materials HashMap almost up to date.
+    // When a `ColorMaterial` is modified we don't remove it from its previous image entry in the HashMap
+    // thus we handle an `outdated` HashSet during modified_images iteration.
+    for event in material_events_reader.iter(&material_events) {
+        match event {
+            AssetEvent::Created { handle } | AssetEvent::Modified { handle } => {
+                if let Some(image) = materials.get(handle).and_then(|mat| mat.texture.as_ref()) {
+                    image_to_material
+                        .entry(image.clone_weak())
+                        .or_default()
+                        .insert(handle.clone_weak());
+                }
+            }
+            AssetEvent::Removed { handle } => {
+                if let Some(image) = materials.get(handle).and_then(|mat| mat.texture.as_ref()) {
+                    image_to_material
+                        .entry(image.clone_weak())
+                        .or_default()
+                        .remove(handle);
+                }
+            }
+        }
+    }
+    let modified_images = image_events
+        .iter()
+        .filter_map(|event| {
+            if let AssetEvent::Modified { handle } = event {
+                Some(handle)
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>();
+    if !modified_images.is_empty() {
+        for image in modified_images.iter() {
+            if let Some(material_handles) = image_to_material.get_mut(image) {
+                let mut outdated = HashSet::default();
+                for material_handle in material_handles.iter() {
+                    if Some(*image)
+                        == materials
+                            .get(material_handle)
+                            .and_then(|mat| mat.texture.as_ref())
+                    {
+                        material_events.send(AssetEvent::Modified {
+                            handle: material_handle.clone_weak(),
+                        })
+                    } else {
+                        outdated.insert(material_handle.clone_weak());
+                    }
+                }
+                *material_handles = &*material_handles - &outdated;
             }
         }
     }
