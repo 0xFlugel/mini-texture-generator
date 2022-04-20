0xFlugel/mini-texture-generator

commit 8f660540f9e7956286be2cff6d4cbbfbd1c9b066

The sidebar is an overlay, never under any pipeline element.

src/util.rs
@@ -253,7 +253,7 @@ pub(crate) fn create_pipeline_element(
             size: element_size,
             effect: effect.clone(),
             color_mesh_bundle: ColorMesh2dBundle {
-                transform: Transform::from_translation(translation.extend(1.0)),
+                transform: Transform::from_translation(translation.extend(0.01)),
                 mesh: element_mesh,
                 material: material.clone(),
                 ..Default::default()

src/main.rs
@@ -673,7 +673,9 @@ fn setup(
     );
     let sidebar = cmds
         .spawn_bundle(ColorMesh2dBundle {
-            transform: Transform::from_translation(sidebar_center.extend(0.0)),
+            /// Sidebar at Z=999 (out of 1000) which is the highest visible position (with children
+            /// having up to +1 relative z position).
+            transform: Transform::from_translation(sidebar_center.extend(999.0)),
             mesh: Mesh2dHandle(mesh_assets.add(Mesh::from(shape::Quad::new(Vec2::new(
                 sidebar_width,
                 sidebar_height,

commit 31285b419ddc60886b5d3a4c27d58ea1c64062c8

Fix texture not being shown when newly created from the sidebar.
Bug introduced in 8f660540f9e7956286be2cff6d4cbbfbd1c9b066.

src/util.rs
@@ -201,7 +201,7 @@ pub(crate) fn create_pipeline_element(
             element_size.0.height / 2.0 + size / 2.0,
             // Raise the texture above all other elements as that is the central part of the entire
             // program.
-            0.9,
+            0.5,
         ));
         let (texture, image_handle) = create_image_entity(
             cmds,